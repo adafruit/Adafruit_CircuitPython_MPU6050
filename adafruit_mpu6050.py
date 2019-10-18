e065b6df5602f46f1e9bc90bf4f871f984c6fcbe
siddacious <nospam187+github@gmail.com>
2019-10-18 14:09:53 -0700

    good for 1.0.0

--- adafruit_mpu6050.py
+++ adafruit_mpu6050.py
@@ -70,6 +70,7 @@ _MPU6050_INT_PIN_CONFIG   = 0x37 # Interrupt pin configuration register
 _MPU6050_ACCEL_OUT        = 0x3B # base address for sensor data reads
 _MPU6050_TEMP_OUT         = 0x41 # Temperature data high byte register
 _MPU6050_GYRO_OUT         = 0x43 # base address for sensor data reads
+_MPU6050_SIG_PATH_RESET   = 0x68 # register to reset sensor signal paths
 _MPU6050_USER_CTRL        = 0x6A # FIFO and I2C Master control register
 _MPU6050_PWR_MGMT_1       = 0x6B # Primary power/sleep control register
 _MPU6050_PWR_MGMT_2       = 0x6C # Secondary power/sleep control register
@@ -79,21 +80,45 @@ STANDARD_GRAVITY = 9.80665
 # pylint: enable=bad-whitespace
 
 class Range: # pylint: disable=too-few-public-methods
-    """Allowed values for `accelerometer_range`."""
+    """Allowed values for `accelerometer_range`.
+
+    - ``Range.RANGE_2_G``
+    - ``Range.RANGE_4_G``
+    - ``Range.RANGE_8_G``
+    - ``Range.RANGE_16_G``
+
+    """
     RANGE_2_G = 0  # +/- 2g (default value)
     RANGE_4_G = 1  # +/- 4g
     RANGE_8_G = 2  # +/- 8g
     RANGE_16_G = 3 # +/- 16g
 
 class GyroRange: # pylint: disable=too-few-public-methods
-    """Allowed values for `gyro_range`."""
-    RANGE_250_DEG = 0  # +/- 250 deg/s (default value)
-    RANGE_500_DEG = 1  # +/- 500 deg/s
-    RANGE_1000_DEG = 2 # +/- 1000 deg/s
-    RANGE_2000_DEG = 3 # +/- 2000 deg/s
+    """Allowed values for `gyro_range`.
+
+    - ``GyroRange.RANGE_250_DPS``
+    - ``GyroRange.RANGE_500_DPS``
+    - ``GyroRange.RANGE_1000_DPS``
+    - ``GyroRange.RANGE_2000_DPS``
+
+    """
+    RANGE_250_DPS = 0  # +/- 250 deg/s (default value)
+    RANGE_500_DPS = 1  # +/- 500 deg/s
+    RANGE_1000_DPS = 2 # +/- 1000 deg/s
+    RANGE_2000_DPS = 3 # +/- 2000 deg/s
 
 class Bandwidth: # pylint: disable=too-few-public-methods
-    """Allowed values for `filter_bandwidth`."""
+    """Allowed values for `filter_bandwidth`.
+
+    - ``Bandwidth.BAND_260_HZ``
+    - ``Bandwidth.BAND_184_HZ``
+    - ``Bandwidth.BAND_94_HZ``
+    - ``Bandwidth.BAND_44_HZ``
+    - ``Bandwidth.BAND_21_HZ``
+    - ``Bandwidth.BAND_10_HZ``
+    - ``Bandwidth.BAND_5_HZ``
+
+    """
     BAND_260_HZ = 0 # Docs imply this disables the filter
     BAND_184_HZ = 1 # 184 Hz
     BAND_94_HZ = 2  # 94 Hz
@@ -103,7 +128,14 @@ class Bandwidth: # pylint: disable=too-few-public-methods
     BAND_5_HZ = 6   # 5 Hz
 
 class Rate: # pylint: disable=too-few-public-methods
-    """Allowed values for `cycle_rate`"""
+    """Allowed values for `cycle_rate`.
+
+    - ``Rate.CYCLE_1_25_HZ``
+    - ``Rate.CYCLE_5_HZ``
+    - ``Rate.CYCLE_20_HZ``
+    - ``Rate.CYCLE_40_HZ``
+
+    """
     CYCLE_1_25_HZ = 0 # 1.25 Hz
     CYCLE_5_HZ = 1    # 5 Hz
     CYCLE_20_HZ = 2   # 20 Hz
@@ -111,8 +143,10 @@ class Rate: # pylint: disable=too-few-public-methods
 
 class MPU6050:
     """Driver for the MPU6050 6-axis accelerometer and gyroscope.
-        :param ~busio.I2C i2c_bus: The I2C bus the MSA is connected to.
+
+        :param ~busio.I2C i2c_bus: The I2C bus the MPU6050 is connected to.
         :param address: The I2C slave address of the sensor
+
     """
     def __init__(self, i2c_bus, address=_MPU6050_DEFAULT_ADDRESS):
         self.i2c_device = i2c_device.I2CDevice(i2c_bus, address)
@@ -122,38 +156,50 @@ class MPU6050:
 
         self.reset()
 
+        self._sample_rate_divisor = 0
+        self._filter_bandwidth = Bandwidth.BAND_260_HZ
+        self._gyro_range = GyroRange.RANGE_500_DPS
+        self._accel_range = Range.RANGE_2_G
+        sleep(0.100)
+        self._clock_source = 1 # set to use gyro x-axis as reference
+        sleep(0.100)
+        self.sleep = False
+        sleep(0.010)
+
+
     def reset(self):
         """Reinitialize the sensor"""
         self._reset = True
         while self._reset is True:
-            sleep(0.10)
+            sleep(0.001)
+        sleep(0.100)
 
-        self._sample_rate_divisor = 0
-        self._gyro_range = GyroRange.RANGE_1000_DEG
-        self._accel_range = Range.RANGE_8_G
-        sleep(0.1)
-        self._filter_bandwidth = Bandwidth.BAND_260_HZ
-        self._clock_source = 1 # set to use gyro x-axis as reference
-        sleep(0.1)
-        self.sleep = False
+        _signal_path_reset = 0b111 # reset all sensors
+        sleep(0.100)
 
+    _clock_source = RWBits(3, _MPU6050_PWR_MGMT_1, 0)
     _device_id = ROUnaryStruct(_MPU6050_WHO_AM_I, ">B")
+
     _reset = RWBit(_MPU6050_PWR_MGMT_1, 7, 1)
-    _sample_rate_divisor = UnaryStruct(_MPU6050_SMPLRT_DIV, ">B")
+    _signal_path_reset = RWBits(3, _MPU6050_SIG_PATH_RESET, 3)
+
     _gyro_range = RWBits(2, _MPU6050_GYRO_CONFIG, 3)
     _accel_range = RWBits(2, _MPU6050_ACCEL_CONFIG, 3)
+
     _filter_bandwidth = RWBits(2, _MPU6050_CONFIG, 3)
-    _clock_source = RWBits(3, _MPU6050_PWR_MGMT_1, 0)
+
     _raw_accel_data = StructArray(_MPU6050_ACCEL_OUT, ">h", 3)
     _raw_gyro_data = StructArray(_MPU6050_GYRO_OUT, ">h", 3)
     _raw_temp_data = ROUnaryStruct(_MPU6050_TEMP_OUT, ">h")
+
     _cycle = RWBit(_MPU6050_PWR_MGMT_1, 5)
     _cycle_rate = RWBits(2, _MPU6050_PWR_MGMT_2, 6, 1)
 
     sleep = RWBit(_MPU6050_PWR_MGMT_1, 6, 1)
     """Shuts down the accelerometers and gyroscopes, saving power. No new data will
     be recorded until the sensor is taken out of sleep by setting to `False`"""
-
+    sample_rate_divisor = UnaryStruct(_MPU6050_SMPLRT_DIV, ">B")
+    """The sample rate divisor. See the datasheet for additional detail"""
 
     @property
     def temperature(self):
@@ -198,13 +244,13 @@ class MPU6050:
 
         gyro_scale = 1
         gyro_range = self._gyro_range
-        if gyro_range == GyroRange.RANGE_250_DEG:
+        if gyro_range == GyroRange.RANGE_250_DPS:
             gyro_scale = 131
-        if gyro_range == GyroRange.RANGE_500_DEG:
+        if gyro_range == GyroRange.RANGE_500_DPS:
             gyro_scale = 65.5
-        if gyro_range == GyroRange.RANGE_1000_DEG:
+        if gyro_range == GyroRange.RANGE_1000_DPS:
             gyro_scale = 32.8
-        if gyro_range == GyroRange.RANGE_2000_DEG:
+        if gyro_range == GyroRange.RANGE_2000_DPS:
             gyro_scale = 16.4
 
         # setup range dependant scaling
--- docs/examples.rst
+++ docs/examples.rst
@@ -6,3 +6,23 @@ Ensure your device works with this simple test.
 .. literalinclude:: ../examples/mpu6050_simpletest.py
     :caption: examples/mpu6050_simpletest.py
     :linenos:
+
+Plotter Example
+---------------
+
+See the effects of changing the gyroscope and accelerometer range
+by viewing the data in a serial plotter
+
+.. literalinclude:: ../examples/mpu6050_plotter_example.py
+    :caption: examples/mpu6050_plotter_example.py
+    :linenos:
+
+Sleep Example
+---------------
+
+Observe how the cycle and sleep modes effect measurements 
+by viewing the data in a serial plotter
+
+.. literalinclude:: ../examples/mpu6050_sleep_example.py
+    :caption: examples/mpu6050_sleep_example.py
+    :linenos:
