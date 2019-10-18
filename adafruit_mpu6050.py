7ec5302ef8ecc82b0df4e7dddef6842592e62bc3
siddacious <nospam187+github@gmail.com>
2019-10-18 15:34:40 -0700

    default addr to 0x68 to match other libs

--- adafruit_mpu6050.py
+++ adafruit_mpu6050.py
@@ -55,7 +55,7 @@ from adafruit_register.i2c_bit import RWBit
 from adafruit_register.i2c_bits import RWBits
 import adafruit_bus_device.i2c_device as i2c_device
 # pylint: disable=bad-whitespace
-_MPU6050_DEFAULT_ADDRESS  = 0x69  # MPU6050 default i2c address w/ AD0 high
+_MPU6050_DEFAULT_ADDRESS  = 0x68  # MPU6050 default i2c address w/ AD0 low
 _MPU6050_DEVICE_ID        = 0x68 # The correct MPU6050_WHO_AM_I value
 
 _MPU6050_SELF_TEST_X      = 0x0D # Self test factory calibrated values register
