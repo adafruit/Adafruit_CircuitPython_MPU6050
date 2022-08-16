061a0e5b0a46d7a3e6f6ffe69c63fe7e289396ab
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:14 -0400

    Update version string

--- adafruit_mpu6050.py
+++ adafruit_mpu6050.py
@@ -33,7 +33,7 @@ Implementation Notes
 
 # imports
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_MPU6050.git"
 
 from math import radians
