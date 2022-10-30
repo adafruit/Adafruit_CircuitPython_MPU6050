087a70fdcc28ec1338053d741c87d8ef21a886b2
SquirtleSquadLeader <112285567+SquirtleSquadLeader@users.noreply.github.com>
2022-10-30 18:38:21 -0400

    Removed test script at bottome
    ... maybe this is it?

--- adafruit_mpu6050.py
+++ adafruit_mpu6050.py
@@ -8,6 +8,10 @@
 
 CircuitPython helper library for the MPU6050 6-DoF Accelerometer and Gyroscope
 
+WARNING: Reference(s) to "Master" / "Slave" used only to match original documentation
+         by the manufacturer, TDK Invnesense.  Please feel free to rename these to
+         something more agreeable like Leader/Follower if you feel so inclined.  
+
 
 * Author(s): Bryan Siepert
 
@@ -29,6 +33,19 @@ Implementation Notes
 
 * Adafruit's Register library:
   https://github.com/adafruit/Adafruit_CircuitPython_Register
+  
+* Registers added without seeing concise documenation on their
+  implementation.  Please use at your own risk until further
+  development allows for safe usage.
+  
+  ** Datasheets, Register Maps, etc... **
+  
+* Datasheet: https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Datasheet1.pdf
+
+* Register Map: https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Register-Map1.pdf
+  
+* Offet Register Document: https://www.digikey.com/en/pdf/i/invensense/mpu-hardware-offset-registers
+
 """
 
 # imports
@@ -50,29 +67,175 @@ try:
 except ImportError:
     pass
 
-_MPU6050_DEFAULT_ADDRESS = 0x68  # MPU6050 default i2c address w/ AD0 low
-_MPU6050_DEVICE_ID = 0x68  # The correct MPU6050_WHO_AM_I value
+_MPU6050_DEVICE_ID_AD0_LO = 0x68
+_MPU6050_DEVICE_ID_AD0_HI = 0x69
 
-_MPU6050_SELF_TEST_X = 0x0D  # Self test factory calibrated values register
-_MPU6050_SELF_TEST_Y = 0x0E  # Self test factory calibrated values register
-_MPU6050_SELF_TEST_Z = 0x0F  # Self test factory calibrated values register
-_MPU6050_SELF_TEST_A = 0x10  # Self test factory calibrated values register
-_MPU6050_SMPLRT_DIV = 0x19  # sample rate divisor register
-_MPU6050_CONFIG = 0x1A  # General configuration register
-_MPU6050_GYRO_CONFIG = 0x1B  # Gyro specfic configuration register
-_MPU6050_ACCEL_CONFIG = 0x1C  # Accelerometer specific configration register
-_MPU6050_INT_PIN_CONFIG = 0x37  # Interrupt pin configuration register
-_MPU6050_ACCEL_OUT = 0x3B  # base address for sensor data reads
-_MPU6050_TEMP_OUT = 0x41  # Temperature data high byte register
-_MPU6050_GYRO_OUT = 0x43  # base address for sensor data reads
-_MPU6050_SIG_PATH_RESET = 0x68  # register to reset sensor signal paths
-_MPU6050_USER_CTRL = 0x6A  # FIFO and I2C Master control register
-_MPU6050_PWR_MGMT_1 = 0x6B  # Primary power/sleep control register
-_MPU6050_PWR_MGMT_2 = 0x6C  # Secondary power/sleep control register
-_MPU6050_WHO_AM_I = 0x75  # Divice ID register
+"""
+_MPU6050_XG_OFFS_TC   = 0x00     
+_MPU6050_YG_OFFS_TC   = 0x01       
+_MPU6050_ZG_OFFS_TC   = 0x02    # FURTHER DEVELOPMENT NEEDED TO ENABLE THESE 
+_MPU6050_FINE_GAIN    = 0x03   
+_MPU6050_Y_FINE_GAIN  = 0x04   
+_MPU6050_Z_FINE_GAIN  = 0x05   
+"""
+
+# Accelerometer Offset Registers
+_MPU6050_XA_OFFS_USRH = 0x06
+_MPU6050_XA_OFFS_USRL = 0x07
+_MPU6050_YA_OFFS_USRH = 0x08
+_MPU6050_YA_OFFS_USRL = 0x09
+_MPU6050_ZA_OFFS_USRH = 0x0A
+_MPU6050_ZA_OFFS_USRL = 0x0B
+  
+# Self Test Registers
+_MPU6050_SELF_TEST_X = 0x0D
+_MPU6050_SELF_TEST_Y = 0x0E
+_MPU6050_SELF_TEST_Z = 0x0F
+_MPU6050_SELF_TEST_A = 0x10
+
+#  Gyroscope Offset Registers
+_MPU6050_XG_OFFS_USRH = 0x13
+_MPU6050_XG_OFFS_USRL = 0x14
+_MPU6050_YG_OFFS_USRH = 0x15
+_MPU6050_YG_OFFS_USRL = 0x16
+_MPU6050_ZG_OFFS_USRH = 0x17
+_MPU6050_ZG_OFFS_USRL = 0x18
+    
+# Configuration Registers
+_MPU6050_SMPLRT_DIV   = 0x19
+_MPU6050_CONFIG       = 0x1A
+_MPU6050_GYRO_CONFIG  = 0x1B
+_MPU6050_ACCEL_CONFIG = 0x1C
+
+"""
+# Freefall 
+_MPU6050_FF_THR = 0x1D  # FURTHER DEVELOPMENT NEEDED TO ENABLE THESE
+_MPU6050_FF_DUR = 0x1E
+
+# Motion Detection
+_MPU6050_MOT_DET_THRSHLD  = 0x1F
+_MPU6050_MOT_DET_DURATION = 0x20
+_MPU6050_ZRMOT_THR        = 0x21   
+_MPU6050_ZRMOT_DUR        = 0x22     
+"""
+
+# FIFO Enable
+_MPU6050_FIFO_EN  = 0x23
+
+"""
+# I2C Master/Slave Bus Control
+_MPU6050_I2C_MST_CTRL    = 0x24 # FURTHER DEVELOPMENT NEEDED TO ENABLE THESE
+_MPU6050_I2C_SLV0_ADDR   = 0x25
+_MPU6050_I2C_SLV0_REG    = 0x26
+_MPU6050_I2C_SLV0_CTRL   = 0x27    
+_MPU6050_I2C_SLV1_ADDR   = 0x28
+_MPU6050_I2C_SLV1_REG    = 0x29
+_MPU6050_I2C_SLV1_CTRL   = 0x2A    
+_MPU6050_I2C_SLV2_ADDR   = 0x2B
+_MPU6050_I2C_SLV2_REG    = 0x2C
+_MPU6050_I2C_SLV2_CTRL   = 0x2D       
+_MPU6050_I2C_SLV3_ADDR   = 0x2E 
+_MPU6050_I2C_SLV3_REG    = 0x2F
+_MPU6050_I2C_SLV3_CTRL   = 0x30    
+_MPU6050_I2C_SLV4_ADDR   = 0x31
+_MPU6050_I2C_SLV4_REG    = 0x32
+_MPU6050_I2C_SLV4_DO     = 0x33
+_MPU6050_I2C_SLV4_CTRL   = 0x34
+_MPU6050_I2C_SLV4_DI     = 0x35   
+_MPU6050_I2C_MST_STATUS  = 0x36
+"""
+
+# Interrupt Configuration
+_MPU6050_INT_PIN_CFG    = 0x37
+_MPU6050_INT_ENABLE     = 0x38
+
+# _DMP_INT_STATUS = 0x39   
+_MPU6050_INT_STATUS     = 0x3A
+    
+# Sensor Outputs
+_MPU6050_ACCEL_XOUT_H = 0x3B
+_MPU6050_ACCEL_XOUT_L = 0x3C
+_MPU6050_ACCEL_YOUT_H = 0x3D
+_MPU6050_ACCEL_YOUT_L = 0x3E
+_MPU6050_ACCEL_ZOUT_H = 0x3F
+_MPU6050_ACCEL_ZOUT_L = 0x40
+_MPU6050_TEMP_OUT_H   = 0x41
+_MPU6050_TEMP_OUT_L   = 0x42
+_MPU6050_GYRO_XOUT_H  = 0x43
+_MPU6050_GYRO_XOUT_L  = 0x44
+_MPU6050_GYRO_YOUT_H  = 0x45
+_MPU6050_GYRO_YOUT_L  = 0x46
+_MPU6050_GYRO_ZOUT_H  = 0x47
+_MPU6050_GYRO_ZOUT_L  = 0x48
+
+"""
+# External Sensor Data  More development needed to enable these
+_MPU6050_EXT_SENS_DATA_00 = 0x49
+_MPU6050_EXT_SENS_DATA_01 = 0x4A
+_MPU6050_EXT_SENS_DATA_02 = 0x4B
+_MPU6050_EXT_SENS_DATA_03 = 0x4C
+_MPU6050_EXT_SENS_DATA_04 = 0x4D
+_MPU6050_EXT_SENS_DATA_05 = 0x4E
+_MPU6050_EXT_SENS_DATA_06 = 0x4F
+_MPU6050_EXT_SENS_DATA_07 = 0x50
+_MPU6050_EXT_SENS_DATA_08 = 0x51
+_MPU6050_EXT_SENS_DATA_09 = 0x52
+_MPU6050_EXT_SENS_DATA_10 = 0x53
+_MPU6050_EXT_SENS_DATA_11 = 0x54
+_MPU6050_EXT_SENS_DATA_12 = 0x55
+_MPU6050_EXT_SENS_DATA_13 = 0x56
+_MPU6050_EXT_SENS_DATA_14 = 0x57
+_MPU6050_EXT_SENS_DATA_15 = 0x58
+_MPU6050_EXT_SENS_DATA_16 = 0x59
+_MPU6050_EXT_SENS_DATA_17 = 0x5A
+_MPU6050_EXT_SENS_DATA_18 = 0x5B
+_MPU6050_EXT_SENS_DATA_19 = 0x5C
+_MPU6050_EXT_SENS_DATA_20 = 0x5D
+_MPU6050_EXT_SENS_DATA_21 = 0x5E
+_MPU6050_EXT_SENS_DATA_22 = 0x5F
+_MPU6050_EXT_SENS_DATA_23 = 0x60
+
+# Motion Detect Status
+# _MPU6050_MOT_DETECT_STATUS = 0x61   
+
+
+# I2C Slave DO
+_MPU6050_I2C_SLV0_DO = 0x63
+_MPU6050_I2C_SLV1_DO = 0x64
+_MPU6050_I2C_SLV2_DO = 0x65
+_MPU6050_I2C_SLV3_DO = 0x66
+        
+# I2C Master Delay Control
+_MPU6050_I2C_MT_DELAY_CTRL = 0x67
+"""
+
+# Resets / Enables
+_MPU6050_SIGNAL_PATH_RESET = 0x68
+
+# _MPU6050_MOT_DETECT_CTRL = 0x69
+
+_MPU6050_USER_CTRL         = 0x6A
+_MPU6050_PWR_MGMT_1        = 0x6B
+_MPU6050_PWR_MGMT_2        = 0x6C
+
+"""
+# DMP
+_MPU6050_BANK_SEL       = const(0x6D)        
+_MPU6050_MEM_START_ADDR = const(0x6E) 
+_MPU6050_MEM_R_W        = const(0x6F)      # FURTHER DEVELOPMENT NEEDED TO ENABLE THESE 
+_MPU6050_DMP_CFG_1      = const(0x70)      
+_MPU6050_DMP_CFG_2      = const(0x71)            
+""" 
+
+# FIFO Buffer Count
+_MPU6050_FIFO_COUNTH = 0x72
+_MPU6050_FIFO_COUNTL = 0x73
+_MPU6050_FIFO_R_W =    0x74
+        
+# Who am I?  "To be... or not to be? That -is- the question"
+_MPU6050_WHO_AM_I = 0x75
 
 STANDARD_GRAVITY = 9.80665
-
 
 class Range:  # pylint: disable=too-few-public-methods
     """Allowed values for `accelerometer_range`.
@@ -178,12 +341,29 @@ class MPU6050:
 
     """
 
-    def __init__(self, i2c_bus: I2C, address: int = _MPU6050_DEFAULT_ADDRESS) -> None:
+    def __init__(self, i2c_bus: I2C, address) -> None:
+        
+        # Check user input for correctness correctness
+        if address is not _MPU6050_DEVICE_ID_AD0_LO and address is not _MPU6050_DEVICE_ID_AD0_HI:
+            raise RuntimeError("MPU6050 address passed in is incorrect. Expecting "
+                "0x68 OR 0x69 (104 OR 105).  See Data Sheet / Register Map for details")
+        
+        # Verify i2c_bus is not already locked to prevent a hang
+        i2c_bus.unlock()            
+         
+        # Attempt to establish an I2CDevice object
         self.i2c_device = i2c_device.I2CDevice(i2c_bus, address)
-
-        if self._device_id != _MPU6050_DEVICE_ID:
-            raise RuntimeError("Failed to find MPU6050 - check your wiring!")
-
+        
+        # Verify WHO_AM_I (register 0x75) responds with value of 104, regardless of AD0 state.
+        # This double checks the device responding is a MPU6050
+        if self._who_am_i[0][0] is _MPU6050_DEVICE_ID_AD0_LO:
+            print("Communication to MPU6050 @", address, " confirmed.")
+        else:
+            raise RuntimeError("MPU6050 @ ", address, " invalid response from \"Who Am I\" "
+                "(104 0x68) not found. Possible a device, other than MPU6050, responding "
+                "from the same address. Check for I2C address conflicts on the same "
+                "I2C bus and try again.")
+        
         self.reset()
 
         self._sample_rate_divisor = 0
@@ -210,17 +390,18 @@ class MPU6050:
     _device_id = ROUnaryStruct(_MPU6050_WHO_AM_I, ">B")
 
     _reset = RWBit(_MPU6050_PWR_MGMT_1, 7, 1)
-    _signal_path_reset = RWBits(3, _MPU6050_SIG_PATH_RESET, 3)
+    _signal_path_reset = RWBits(3, _MPU6050_SIGNAL_PATH_RESET, 3)
 
     _gyro_range = RWBits(2, _MPU6050_GYRO_CONFIG, 3)
     _accel_range = RWBits(2, _MPU6050_ACCEL_CONFIG, 3)
 
     _filter_bandwidth = RWBits(2, _MPU6050_CONFIG, 3)
 
-    _raw_accel_data = StructArray(_MPU6050_ACCEL_OUT, ">h", 3)
-    _raw_gyro_data = StructArray(_MPU6050_GYRO_OUT, ">h", 3)
-    _raw_temp_data = ROUnaryStruct(_MPU6050_TEMP_OUT, ">h")
-
+    _raw_accel_data = StructArray(_MPU6050_ACCEL_XOUT_H, ">h", 3)
+    _raw_gyro_data = StructArray(_MPU6050_GYRO_XOUT_H, ">h", 3)
+    _raw_temp_data = ROUnaryStruct(_MPU6050_TEMP_OUT_H, ">h")
+    
+    _who_am_i = StructArray(_MPU6050_WHO_AM_I,'B',1)
     _cycle = RWBit(_MPU6050_PWR_MGMT_1, 5)
     _cycle_rate = RWBits(2, _MPU6050_PWR_MGMT_2, 6, 1)
 
