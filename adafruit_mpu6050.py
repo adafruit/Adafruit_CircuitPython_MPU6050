78f8ec7efb28cdea4446d50275ce2889d2eb2a1d
SquirtleSquadLeader <112285567+SquirtleSquadLeader@users.noreply.github.com>
2022-11-02 20:48:23 -0400

    try again
    Sorry.

--- adafruit_mpu6050.py
+++ adafruit_mpu6050.py
@@ -238,7 +238,7 @@ STANDARD_GRAVITY = 9.80665
 
 
 class Clksel:  # pylint: disable=too-few-public-methods
-    """Allowed values for `_clock_source` Register object.
+    """Allowed values for `_clksel` Register object.
 
     * :attr:'Clksel.CLKSEL_INTERNAL_8MHz
     * :attr:'Clksel.CLKSEL_INTERNAL_X
