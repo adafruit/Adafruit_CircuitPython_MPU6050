c3fec5727c2eb4462200ca2b006a7650bc74fe79
dherrada <dylan.herrada@adafruit.com>
2022-11-18 13:05:27 -0500

    Added commented out board.STEMMA_I2C with explanation

--- examples/mpu6050_inclinometer.py
+++ examples/mpu6050_inclinometer.py
@@ -12,6 +12,7 @@ import board
 import adafruit_mpu6050
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 sensor = adafruit_mpu6050.MPU6050(i2c)
 
 
